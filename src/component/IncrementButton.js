5e6f6dc8b15fd59e1b5a263f2d50ce1119901fd6
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-08-06 18:59:17 +0100

    Remove comments

--- src/component/IncrementButton.js
+++ src/component/IncrementButton.js
@@ -1,21 +1,5 @@
 import { Component } from "react";
 
-/*
-Using what you’ve learnt from the previous
-slides, add the appropriate lifecycle method/s to
-meet the requirement below:
-
-I want there to be a console output denoting that
-the component successfully exists / came onto
-the screen
-
-I want a console output listing the newest value
-
-everytime I click on my Increment Button
-counter (where the state changes and updates
-each time) - use a lifecycle method for this
-*/
-
 export default class IncrementButton extends Component {
   state = {
     count: 0
